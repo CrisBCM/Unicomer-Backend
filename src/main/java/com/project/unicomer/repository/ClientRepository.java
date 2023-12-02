1914dcc306e65f7b1a6969da4052d13c0d7a33d1
CrisBCM <cristianbcm1999@gmail.com>
2023-12-01 23:14:48 -0300

    Feat: Add configuration of JwtAuthenticationFilter and JwtService

---
+++ src/main/java/com/project/unicomer/repository/ClientRepository.java
@@ -0,0 +1,13 @@
+package com.project.unicomer.repository;
+
+import com.project.unicomer.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByDni(String dni);
+
+}
